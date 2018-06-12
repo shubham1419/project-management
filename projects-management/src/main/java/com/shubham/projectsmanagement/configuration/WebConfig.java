b177cb173751d3542cde077bfb79dc7534eff117
Shubham Gupta <shubham.iquincesoft@gmail.com>
2018-06-12 16:34:35 +0530

    few configuration related changes

---
+++ projects-management/src/main/java/com/shubham/projectsmanagement/configuration/WebConfig.java
@@ -0,0 +1,7 @@
+package com.shubham.projectsmanagement.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+}
