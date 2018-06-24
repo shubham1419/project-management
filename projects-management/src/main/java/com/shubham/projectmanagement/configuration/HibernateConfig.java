fa192147a331837bfa3a3b6a73cd2e1c7e36f81e
Shubham <Shubham@Hina>
2018-06-24 12:13:40 +0530

    Update Configuration Files

--- projects-management/src/main/java/com/shubham/projectmanagement/configuration/HibernateConfig.java
+++ projects-management/src/main/java/com/shubham/projectmanagement/configuration/HibernateConfig.java
@@ -1,64 +1,38 @@
 package com.shubham.projectmanagement.configuration;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.shubham.projectmanagement.dto.Developer;
 
-
 @Configuration
-@PropertySource("classpath:db.properties")
 @EnableTransactionManagement
-@ComponentScans(value = { 
-	      @ComponentScan("com.shubham.projectmanagement")
-	    })
-
+@ComponentScans(value = { @ComponentScan("com.shubham.projectmanagement")})
 public class HibernateConfig {
-	
+
 	@Autowired
-	   private Environment env;
+	private ApplicationContext context;
 
-	   @Bean("dataSource")
-	   public DataSource getDataSource() {
-	      BasicDataSource dataSource = new BasicDataSource();
-	      dataSource.setDriverClassName(env.getProperty("db.driver"));
-	      dataSource.setUrl(env.getProperty("db.url"));
-	      dataSource.setUsername(env.getProperty("db.username"));
-	      dataSource.setPassword(env.getProperty(""));
-	      return dataSource;
-	   }
+	@Bean
+	public LocalSessionFactoryBean getSessionFactory() {
+		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+		factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
+		factoryBean.setAnnotatedClasses(Developer.class);
+		return factoryBean;
+	}
 
-	   @Bean
-	   public LocalSessionFactoryBean getSessionFactory() {
-	      LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-	      factoryBean.setDataSource(getDataSource());
-	      
-	      Properties props=new Properties();
-	      props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-	      props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+	@Bean
+	public HibernateTransactionManager getTransactionManager() {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(getSessionFactory().getObject());
+		return transactionManager;
+	}
 
-	      factoryBean.setHibernateProperties(props);
-	      factoryBean.setAnnotatedClasses(Developer.class );
-	      return factoryBean;
-	   }
-
-	   @Bean
-	   public HibernateTransactionManager getTransactionManager() {
-	      HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-	      transactionManager.setSessionFactory(getSessionFactory().getObject());
-	      return transactionManager;
-	   }
 }
