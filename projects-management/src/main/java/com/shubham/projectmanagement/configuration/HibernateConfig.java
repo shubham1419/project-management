6fe8263cda0b5f6ff15c313d986497baa19c4907
sumit <sumit@shubham-inspiron-3647>
2018-06-25 12:59:37 +0530

    Fix minor bugs

--- projects-management/src/main/java/com/shubham/projectmanagement/configuration/HibernateConfig.java
+++ projects-management/src/main/java/com/shubham/projectmanagement/configuration/HibernateConfig.java
@@ -1,38 +1,66 @@
 package com.shubham.projectmanagement.configuration;
 
+
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.shubham.projectmanagement.dto.Developer;
 
+import static org.hibernate.cfg.Environment.*;
+
 @Configuration
+@PropertySource("classpath:dbc3p0.properties")
 @EnableTransactionManagement
-@ComponentScans(value = { @ComponentScan("com.shubham.projectmanagement.dao"),@ComponentScan("com.shubham.projectmanagement.dao"),@ComponentScan("com.shubham.projectmanagement.daoImpl")})
+@ComponentScans(value = { @ComponentScan("com.shubham.projectmanagement.dao"),
+    @ComponentScan("com.shubham.projectmanagement.daoImpl") })
 public class HibernateConfig {
 
-	@Autowired
-	private ApplicationContext context;
+  @Autowired
+  private Environment env;
 
-	@Bean
-	public LocalSessionFactoryBean getSessionFactory() {
-		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-		factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
-		factoryBean.setAnnotatedClasses(Developer.class);
-		return factoryBean;
-	}
+  @Bean
+  public LocalSessionFactoryBean getSessionFactory() {
+    LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+    
+    Properties props = new Properties();
+    
+    // Setting JDBC properties
+    props.put(DRIVER, env.getProperty("mysql.driver"));
+    props.put(URL, env.getProperty("mysql.jdbcUrl"));
+    props.put(USER, env.getProperty("mysql.username"));
+    //props.put(PASS, env.getProperty("mysql.password"));
 
-	@Bean
-	public HibernateTransactionManager getTransactionManager() {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-		transactionManager.setSessionFactory(getSessionFactory().getObject());
-		return transactionManager;
-	}
+    // Setting Hibernate properties
+    props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
+    props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
 
+    // Setting C3P0 properties
+    props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
+    props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
+    props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
+    props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
+    props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
+
+    factoryBean.setHibernateProperties(props);
+    factoryBean.setAnnotatedClasses(Developer.class);
+    
+    return factoryBean;
+  }
+
+  @Bean
+  public HibernateTransactionManager getTransactionManager() {
+    HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+    transactionManager.setSessionFactory(getSessionFactory().getObject());
+    return transactionManager;
+  }
 }
